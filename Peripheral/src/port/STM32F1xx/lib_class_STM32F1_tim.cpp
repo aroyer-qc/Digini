f2737873d5a90d6d2e438d4ad161d6914d8872e4
Alain Royer <Alain.Royer@mttservicecenter.com>
2024-12-19 15:46:13 -0500

    minor FIX F1

--- Peripheral/src/port/STM32F1xx/lib_class_STM32F1_tim.cpp
+++ Peripheral/src/port/STM32F1xx/lib_class_STM32F1_tim.cpp
@@ -93,7 +93,7 @@ void TIM_Driver::Initialize(void)
     // Set the update interrupt enable
     if((m_pInfo->IRQ_DMA_SourceEnable & (TIM_IRQ_UPDATE | TIM_DMA_UPDATE)) != 0)
     {
-         CLEAR_BIT(m_pTim)->SR, TIM_SR_UIF);
+        CLEAR_BIT(m_pTim->SR, TIM_SR_UIF);
         m_pTim->DIER = TIM_DIER_UIE;
     }
 

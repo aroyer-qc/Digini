9974b504bc223ce0d2b74b634890a60efa41cbb8
Alain Royer <Alain.Royer@mttservicecenter.com>
2025-12-19 13:02:39 -0500

    wip H7

--- Peripheral/inc/port/STM32H7xx/lib_class_STM32H7_eth.h
+++ Peripheral/inc/port/STM32H7xx/lib_class_STM32H7_eth.h
@@ -48,10 +48,115 @@
 // Define(s)
 //-------------------------------------------------------------------------------------------------
 
-#define NUM_TX_Buffer               4
-#define NUM_RX_Buffer               6
-#define ETH_BUF_SIZE                1534
-#define ETH_IRQ_PRIO                4
+#define NUM_TX_BUFFER                           4
+#define NUM_RX_BUFFER                           4
+#define ETH_BUF_SIZE                            1536
+#define ETH_IRQ_PRIO                            4
+
+//-------------------------------------------------------------------------------------------------
+// MACCR configuration register
+// Note(s)      if xxx_CFG is not defined default is applied.
+//              The following are not used or not configurable
+//                  - loopback
+//                  - Duplex mode is always set to full
+//                  - Speed is always set to 100M so not available
+//                  - Automatic Pad or CRC Stripping
+//                  - Inter Packet Gap
+//                  - Preamble Length 
+//                  - Watchdog
+//                  - Jabber
+//                      those are mainly for Half duplex
+//                  - Carrier sense before transmission
+//                  - Carrier Sense During Transmission
+//                  - Receive own
+//                  - DeferralCheck
+//                  - Back Off Limit
+    
+// ETH Source Addr Control                      (Use this in ip_cfg for define ETH_SOURCE_ADDRESS_CFG)
+#define ETH_SOURCE_ADDRESS_DISABLE              0x00000000
+#define ETH_SOURCE_ADDRESS_INSERT_ADDR0         ETH_MACCR_SARC_INSADDR0
+#define ETH_SOURCE_ADDRESS_INSERT_ADDR1         ETH_MACCR_SARC_INSADDR1
+#define ETH_SOURCE_ADDRESS_REPLACE_ADDR0        ETH_MACCR_SARC_REPADDR0
+#define ETH_SOURCE_ADDRESS_REPLACE_ADDR1        ETH_MACCR_SARC_REPADDR1
+
+// ETH Checksum Offload                         (Use this in ip_cfg for define ETH_CHECKSUM_OFFLOAD_CFG)
+#define ETH_CHECKSUM_OFFLOAD_DISABLE            0x00000000
+#define ETH_CHECKSUM_OFFLOAD_ENABLE             ETH_MACCR_IPC
+
+// ETH ARP Offload                              (Use this in ip_cfg for define ETH_ARP_OFFLOAD_CFG)
+#define ETH_ARP_OFFLOAD_DISABLE                 0x00000000
+#define ETH_ARP_OFFLOAD_ENABLE                  ETH_MACCR_ARP
+
+
+#if 0
+
+from IA
+ETH_MACConfigTypeDef macConf;
+
+/* Zero the struct to avoid CubeMX garbage */
+memset(&macConf, 0, sizeof(macConf));
+
+
+/* Filtering */
+macConf.PromiscuousMode          = ETH_PROMISCUOUS_MODE_DISABLE;
+macConf.BroadcastFramesReception = ETH_BROADCASTFRAMES_ENABLE;
+macConf.MulticastFramesFilter    = ETH_MULTICASTFRAMESFILTER_PERFECT;
+
+/* Hash table disabled unless you use it */
+macConf.HashTableHigh            = 0;
+macConf.HashTableLow             = 0;
+
+/* Flow control (optional — usually off for embedded) */
+macConf.ReceiveFlowControl       = ETH_RXFLOWCTRL_DISABLE;
+macConf.TransmitFlowControl      = ETH_TXFLOWCTRL_DISABLE;
+
+/* Apply the configuration */
+ETH_SetMACConfig(heth, &macConf);
+
+
+from ST
+
+/*--------------- ETHERNET MAC registers default Configuration --------------*/
+  macDefaultConf.DropTCPIPChecksumErrorPacket = ENABLE;
+  macDefaultConf.ForwardRxErrorPacket = DISABLE;
+  macDefaultConf.ForwardRxUndersizedGoodPacket = DISABLE;
+  macDefaultConf.PauseLowThreshold = ETH_PAUSELOWTHRESHOLD_MINUS_4;
+  macDefaultConf.ReceiveFlowControl = DISABLE;
+  macDefaultConf.ReceiveQueueMode = ETH_RECEIVESTOREFORWARD;
+  macDefaultConf.RetryTransmission = ENABLE;
+  macDefaultConf.TransmitQueueMode = ETH_TRANSMITSTOREFORWARD;
+  macDefaultConf.TransmitFlowControl = DISABLE;
+  macDefaultConf.UnicastPausePacketDetect = DISABLE;
+
+  /* MAC default configuration */
+  ETH_SetMACConfig(heth, &macDefaultConf);
+
+  /*--------------- ETHERNET DMA registers default Configuration --------------*/
+  dmaDefaultConf.AddressAlignedBeats = ENABLE;
+  dmaDefaultConf.BurstMode = ETH_BURSTLENGTH_FIXED;
+  dmaDefaultConf.DMAArbitration = ETH_DMAARBITRATION_RX1_TX1;
+  dmaDefaultConf.FlushRxPacket = DISABLE;
+  dmaDefaultConf.PBLx8Mode = DISABLE;
+  dmaDefaultConf.RebuildINCRxBurst = DISABLE;
+  dmaDefaultConf.RxDMABurstLength = ETH_RXDMABURSTLENGTH_32BEAT;
+  dmaDefaultConf.SecondPacketOperate = DISABLE;
+  dmaDefaultConf.TxDMABurstLength = ETH_TXDMABURSTLENGTH_32BEAT;
+  dmaDefaultConf.TCPSegmentation = DISABLE;
+  dmaDefaultConf.MaximumSegmentSize = ETH_SEGMENT_SIZE_DEFAULT;
+
+  /* DMA default configuration */
+  ETH_SetDMAConfig(heth, &dmaDefaultConf);
+
+#endif
+
+
+
+
+
+
+
+
+
 
 //----- Ethernet MAC Frame Transmit Flags -----
 #define ETH_MAC_TX_FRAME_FRAGMENT   (1UL << 0)      // Indicate frame fragment
@@ -75,44 +180,57 @@
 typedef void (*ETH_MAC_SignalEvent_t) (uint32_t Event);  // Pointer to ETH_MAC_SignalEvent fucntion
 
 // EMAC Driver Control Information
-struct ETH_MAC_Control_t
+struct ETH_MAC_Control_t            // on deathrow probably
 {
-    ETH_MAC_SignalEvent_t   CallbackEvent;          // Event callback
-    uint8_t                 TX_Index;               // Transmit descriptor index
-    uint8_t                 RX_Index;               // Receive descriptor index
+    ETH_MAC_SignalEvent_t   CallbackEvent;                  // Event callback
+    uint8_t                 TX_Index;                       // Transmit descriptor index
+    uint8_t                 RX_Index;                       // Receive descriptor index
   #if (ETH_USE_TIME_STAMP == DEF_ENABLED)
-    uint8_t                 TX_TS_Index;            // Transmit Timestamp descriptor index
+    uint8_t                 TX_TS_Index;                    // Transmit Timestamp descriptor index
   #endif
-    uint8_t*                FrameEnd;               // End of assembled frame fragments
+    uint8_t*                FrameEnd;                       // End of assembled frame fragments
 };
 
-// DMA RX Descriptor
-struct RX_Descriptor_t
+struct ETH_DMA_Descriptor_t
 {
-    uint32_t volatile       Stat;
-    uint32_t                Ctrl;
-    uint8_t const*          Addr;
-    struct RX_Descriptor_t* Next;
-#if ((ETH_USE_CHECKSUM_OFFLOAD == DEF_ENABLED) || (ETH_USE_TIME_STAMP == DEF_ENABLED))
-    uint32_t                ExtStat;
-    uint32_t                Reserved[1];
-    uint32_t                TimeLo;
-    uint32_t                TimeHi;
-#endif
+    volatile uint32_t   Descriptor0;
+    volatile uint32_t   Descriptor1;
+    volatile uint32_t   Descriptor2;
+    volatile uint32_t   Descriptor3;
+    uint32_t            BackupAddress0;                     // Used to store rx buffer 1 address
+    uint32_t            BackupAddress1;                     // Used to store rx buffer 2 address
 };
 
-// DMA TX Descriptor
-struct TX_Descriptor_t
+struct ETH_RX_DescriptorList_t
 {
-    uint32_t volatile       Stat;
-    uint32_t                Size;
-    uint8_t*                Addr;
-    struct TX_Descriptor_t* Next;
-#if ((ETH_USE_CHECKSUM_OFFLOAD == DEF_ENABLED) || (ETH_USE_TIME_STAMP == DEF_ENABLED))
-    uint32_t                Reserved[2];
-    uint32_t                TimeLo;
-    uint32_t                TimeHi;
-#endif
+    uint32_t            Descriptor[NUM_RX_BUFFER];          // RX DMA descriptors addresses.
+    uint32_t            ItMode;                             // If 1, DMA will generate the Rx complete interrupt. If 0, DMA will not generate the Rx complete interrupt.
+    uint32_t            DescriptorIndex;                    // Current RX descriptor.
+    uint32_t            DescriptorCount;                    // Number of descriptors.
+    uint32_t            DataLength;                         // Received data length.
+    uint32_t            BuildDescriptorIndex;               // Current RX Descriptor for building descriptors.
+    uint32_t            BuildDescriptorCount;               // Number of Rx Descriptors awaiting building.
+    uint32_t            LastReceivedDescriptor;             // Last received descriptor.
+    ETH_TimeStamp_t     TimeStamp;                          // Time Stamp low value for receive.
+    void*               pRX_Start;                          // Pointer to the first buffer.
+    void*               pRX_End;                            // Pointer to the last buffer.
+};
+
+struct ETH_TX_DescriptorList_t
+{
+    uint32_t            Descriptor[NUM_TX_BUFFER];          // TX DMA descriptors addresses
+    uint32_t            CurrentDescriptor;                  // Current TX descriptor index for packet transmission
+    uint32_t*           pPacketAddress[ETH_TX_DESC_CNT];    // Ethernet packet addresses array
+    uint32_t*           pCurrentPacketAddress;              // Current transmit packet addresses
+    uint32_t            BuffersInUse;                       // Buffers in use
+    uint32_t            releaseIndex;                       // Release index
+};
+
+struct ETH_Buffer_t
+{
+    uint8_t*            pBuffer;                            // Buffer address
+    uint32_t            Length;                             // Buffer length
+    ETH_Buffer_t*       pNext;                              // Pointer to the next buffer in the list
 };
 
 //-------------------------------------------------------------------------------------------------
@@ -174,10 +292,10 @@ class ETH_Driver : public MAC_DriverInterface
         SystemState_e           PHY_Busy                (void);
 
         static     ETH_MAC_Control_t           m_MAC_Control;
-        static     RX_Descriptor_t             m_RX_Descriptor   [NUM_RX_Buffer]                     __attribute__((section(".RX_DescriptorSection"), aligned(4)));   // Ethernet RX & TX DMA Descriptors
-        static     TX_Descriptor_t             m_TX_Descriptor   [NUM_TX_Buffer]                     __attribute__((section(".TX_DescriptorSection"), aligned(4)));
-        static     uint32_t                    m_RX_Buffer       [NUM_RX_Buffer][ETH_BUF_SIZE >> 2]  __attribute__((section(".RX_ArraySection"),      aligned(4)));   // Ethernet Receive buffers
-        static     uint32_t                    m_TX_Buffer       [NUM_TX_Buffer][ETH_BUF_SIZE >> 2]  __attribute__((section(".TX_ArraySection"),      aligned(4)));   // Ethernet Transmit buffers
+        static     RX_Descriptor_t             m_RX_Descriptor   [NUM_RX_BUFFER]                     __attribute__((section(".RX_DescriptorSection"), aligned(4)));   // Ethernet RX & TX DMA Descriptors
+        static     TX_Descriptor_t             m_TX_Descriptor   [NUM_TX_BUFFER]                     __attribute__((section(".TX_DescriptorSection"), aligned(4)));
+        static     uint32_t                    m_RX_Buffer       [NUM_RX_BUFFER][ETH_BUF_SIZE >> 2]  __attribute__((section(".RX_ArraySection"),      aligned(4)));   // Ethernet Receive buffers
+        static     uint32_t                    m_TX_Buffer       [NUM_TX_BUFFER][ETH_BUF_SIZE >> 2]  __attribute__((section(".TX_ArraySection"),      aligned(4)));   // Ethernet Transmit buffers
 };
 
 //-------------------------------------------------------------------------------------------------

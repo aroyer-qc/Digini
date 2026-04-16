10926aa6166bcf813c2bf36414f04d06c0482017
Alain Royer <Alain.Royer@mttservicecenter.com>
2026-04-16 13:07:16 -0400

    wip

--- Grafx/src/driver/STM32/lib_STM32_fmc_lcd_320x240 - UMSH_8252MD_T - SSD2119.cpp
+++ Grafx/src/driver/STM32/lib_STM32_fmc_lcd_320x240 - UMSH_8252MD_T - SSD2119.cpp
@@ -204,11 +204,8 @@ void GrafxDriver::DrawRectangle(Box_t* pBox)
         DisplayLayer* pLayer;
 
         pLayer = &LayerTable[DisplayLayer::GetDrawing()];
-
-
-        Color       = pLayer->GetColor();
+        Color  = pLayer->GetColor();
         SetWindow(pBox);
-
         Size = pBox->Size.Width * pBox->Size.Height;
 
         for(uint32_t i = 0; i < Size; i++)
@@ -657,25 +654,19 @@ void GrafxDriver::CopyWidgetToDevice(BoxSize_t BoxSize, Cartesian_t Position)
 
     uint16_t* pImageDestination = (uint16_t*)pMemoryPool->Alloc(ImageSize * sizeof(uint16_t), MEM_DBG_GRAFX_CL1);
 
-    DMA2D->CR      = DMA2D_M2M_PFC;                                                                                             // Memory-to-Memory with Pixel Format Conversion
+    DMA2D->CR      = DMA2D_M2M_PFC;                                 // Memory-to-Memory with Pixel Format Conversion
+    DMA2D->FGMAR   = pAddress;                                      // Source address
+    DMA2D->FGOR    = (uint32_t)pLayer->GetSize().X - SizeX;         // Source line offset so none as we are linear
+    DMA2D->FGPFCCR = DMA2D_CONVERSION_ARGB8888;                     // Defines the size of pixel. 0 for format PIXEL_FORMAT_ARGB8888
+    DMA2D->OMAR    = pImageDestination;                             // Destination address
+    DMA2D->OOR     = 0;                                             // Destination line offset none as we are linear
+    DMA2D->OPFCCR  = DMA2D_CONVERSION_RGB565;                       // Defines the size of pixel. 0 for format PIXEL_FORMAT_ARGB8888
+    DMA2D->NLR     = (SizeX << 16) | SizeY;                         // Size configuration of area to be transfered
 
-    //Source
-    DMA2D->FGMAR   = pAddress;       // Source address
-    DMA2D->FGOR    = (uint32_t)pLayer->GetSize().X - SizeX;                                                          // Source line offset so none as we are linear
-    DMA2D->FGPFCCR = DMA2D_CONVERSION_ARGB8888;                                                                                   // Defines the size of pixel. 0 for format PIXEL_FORMAT_ARGB8888
-
-    //Destination
-    DMA2D->OMAR    = pImageDestination;                                                                                       // Destination address
-    DMA2D->OOR     = 0;                                                                               // Destination line offset none as we are linear
-    DMA2D->OPFCCR  = DMA2D_CONVERSION_RGB565;                                                                                   // Defines the size of pixel. 0 for format PIXEL_FORMAT_ARGB8888
-
-    DMA2D->NLR     = (SizeX << 16) | SizeY;                                                                                    // Size configuration of area to be transfered
-
-    SET_BIT(DMA2D->CR, DMA2D_CR_START);                                                                                         // Start operation
-    while(DMA2D->CR & DMA2D_CR_START);                                                                                          // Wait until transfer is done
+    SET_BIT(DMA2D->CR, DMA2D_CR_START);                             // Start operation
+    while(DMA2D->CR & DMA2D_CR_START);                              // Wait until transfer is done
 
     SetWindow(Position.X, Position.Y, &BoxSize);
-
     pDataPtr = pImageDestination;
 
     for(uint32_t i = 0; i < ImageSize; i++)

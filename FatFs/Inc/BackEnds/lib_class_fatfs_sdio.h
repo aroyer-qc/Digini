142ea092d1f2a08e2cbff56ddfc36c05d0067910
Alain Royer <Alain.Royer@mttservicecenter.com>
2026-05-29 12:44:33 -0400

    move sdio into common as it is not link to platform

--- FatFs/Inc/BackEnds/lib_class_fatfs_sdio.h
+++ FatFs/Inc/BackEnds/lib_class_fatfs_sdio.h
@@ -1,6 +1,6 @@
 //-------------------------------------------------------------------------------------------------
 //
-//  File : lib_class_fatfs_sdio.h
+//  File : lib_class_STM32F4_fatfs_sdio.h
 //
 //-------------------------------------------------------------------------------------------------
 //
@@ -24,12 +24,59 @@
 //
 //-------------------------------------------------------------------------------------------------
 
-#if defined STM32F1xx
- #include "./FatFs/Inc/BackEnds/port/STM32F1xx/lib_class_STM32F1_fatfs_sdio.h"
-#elif defined STM32F4xx
- #include "./FatFs/Inc/BackEnds/port/STM32F4xx/lib_class_STM32F4_fatfs_sdio.h"
-#elif defined STM32F7xx
- #include "./FatFs/Inc/BackEnds/port/STM32F7xx/lib_class_STM32F7_fatfs_sdio.h"
-#elif defined STM32H7xx
- #include "./FatFs/Inc/BackEnds/port/STM32H7xx/lib_class_STM32H7_fatfs_sdio.h"
-#endif
+#pragma once
+
+//-------------------------------------------------------------------------------------------------
+// Define(s)
+//-------------------------------------------------------------------------------------------------
+
+#ifdef __cplusplus
+
+//-------------------------------------------------------------------------------------------------
+// Include file(s)
+//-------------------------------------------------------------------------------------------------
+
+//#include "ff.h"
+//#include "diskio.h"
+#include "diskio_interface.h"
+
+//-------------------------------------------------------------------------------------------------
+
+#if (DIGINI_FATFS_USE_SDIO_SD_CARD == DEF_ENABLED)
+
+//-------------------------------------------------------------------------------------------------
+// class definition(s)
+//-------------------------------------------------------------------------------------------------
+
+class CFatFS_SDIO : public DiskIO_DeviceInterface
+{
+
+                        CFatFS_SDIO         ();
+                       ~CFatFS_SDIO         (){}
+
+        DSTATUS         Initialize          (void);
+        DSTATUS         Status              (void);
+        DRESULT         Read                (uint8_t* pBuffer, uint32_t Sector, uint16_t NumberOfSectors);
+        DRESULT         Write               (const uint8_t* pBuffer, uint32_t Sector, uint16_t NumberOfSectors);
+        DRESULT         IO_Ctrl             (uint8_t Control, void* pBuffer);
+        void            Configure           (uint8_t* pBuffer, size_t Size);
+
+    private:
+
+        DRESULT         CheckError          (uint32_t Sector, uint16_t NumberOfSectors);
+
+        bool                        m_IsItInitialize;
+        DSTATUS                     m_Status;
+        uint8_t*                    m_pBuffer;
+        size_t                      m_Size;                       // size of the disk, is a multiple of 512
+};
+
+//-------------------------------------------------------------------------------------------------
+// Global variable(s) and constant(s)
+//-------------------------------------------------------------------------------------------------
+
+#endif // (DIGINI_FATFS_USE_SDIO_SD_CARD == DEF_ENABLED)
+
+//-------------------------------------------------------------------------------------------------
+
+#endif // __cplusplus
